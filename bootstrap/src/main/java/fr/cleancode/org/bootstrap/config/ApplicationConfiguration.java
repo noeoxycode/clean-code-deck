d0abcb1570e090b3810461af8243f7e880374d3d
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2023-02-07 19:48:58 +0100

    configuration[feature/create_hero](clean-code-deck) fix bean problem on the repository

--- bootstrap/src/main/java/fr/cleancode/org/bootstrap/config/ApplicationConfiguration.java
+++ bootstrap/src/main/java/fr/cleancode/org/bootstrap/config/ApplicationConfiguration.java
@@ -2,14 +2,14 @@ package fr.cleancode.org.bootstrap.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.cleancode.org.bootstrap.config.domain.DomainConfiguration;
-import fr.cleancode.org.server.postgres.repository.HeroRepository;
+import fr.cleancode.org.server.postgres.config.PostgresServerAdapterConfiguration;
 import io.vavr.jackson.datatype.VavrModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({DomainConfiguration.class})
+@Import({DomainConfiguration.class, PostgresServerAdapterConfiguration.class})
 public class ApplicationConfiguration {
 
     @Bean
