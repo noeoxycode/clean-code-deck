bb820cded9a781d77fc519a5a7f85ee84ec2396a
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2023-02-14 17:12:02 +0100

    fix-before-merge[feature/create-player](clean-code-deck) remove spring data-jpa because of nosql database implementation

--- server/mongo-adapter/src/main/java/fr/cleancode/org/server/mongo/entities/HeroEntity.java
+++ server/mongo-adapter/src/main/java/fr/cleancode/org/server/mongo/entities/HeroEntity.java
@@ -4,12 +4,9 @@ import fr.cleancode.org.domain.hero.functional.model.Rarity;
 import fr.cleancode.org.domain.hero.functional.model.Speciality;
 import lombok.*;
 import lombok.EqualsAndHashCode.Include;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
 import java.util.UUID;
 
 @Getter
@@ -23,54 +20,13 @@ public class HeroEntity {
 
     @Id
     @Include
-    @Column(
-            name = "heroId",
-            updatable = false,
-            nullable = false,
-            unique = true
-    )
     private UUID heroId;
-    @Column(
-            name = "name",
-            nullable = false,
-            unique = true
-    )
     private String name;
-    @Column(
-            name = "healthPoints",
-            nullable = false
-    )
     private int healthPoints;
-    @Column(
-            name = "currentExperiences",
-            nullable = false
-    )
     private int currentExperiences;
-    @Column(
-            name = "power",
-            nullable = false
-    )
     private int power;
-    @Column(
-            name = "armor",
-            nullable = false
-    )
     private int armor;
-    @Enumerated(EnumType.STRING)
-    @Column(
-            name = "speciality",
-            nullable = false
-    )
     private Speciality speciality;
-    @Enumerated(EnumType.STRING)
-    @Column(
-            name = "rarity",
-            nullable = false
-    )
     private Rarity rarity;
-    @Column(
-            name = "level",
-            nullable = false
-    )
     private int level;
 }
--- server/mongo-adapter/src/main/java/fr/cleancode/org/server/mongo/entities/PlayerEntity.java
+++ server/mongo-adapter/src/main/java/fr/cleancode/org/server/mongo/entities/PlayerEntity.java
@@ -3,11 +3,9 @@ package fr.cleancode.org.server.mongo.entities;
 import fr.cleancode.org.domain.hero.functional.model.Hero;
 import lombok.*;
 import lombok.EqualsAndHashCode.Include;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Id;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,28 +20,8 @@ public class PlayerEntity {
 
     @Id
     @Include
-    @Column(
-            name = "playerId",
-            unique = true,
-            nullable = false,
-            updatable = false
-    )
     private UUID playerId;
-    @Column(
-            name = "pseudo",
-            nullable = false
-    )
     private String pseudo;
-    @Column(
-            name = "token",
-            nullable = false
-    )
     private int token;
-    @Column(
-            name = "deck",
-            nullable = false,
-            unique = true
-    )
-    @ElementCollection
     private List<Hero> deck;
 }
