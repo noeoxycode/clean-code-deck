3cb2832486d636944d751658240d4a21015f6ef9
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-02-16 21:43:24 +0100

    development[feature/buyPack](clean-code-deck) patching by adding PaymentException

--- domain/src/main/java/fr/cleancode/org/domain/pack/functional/service/Buying.java
+++ domain/src/main/java/fr/cleancode/org/domain/pack/functional/service/Buying.java
@@ -1,17 +1,20 @@
 package fr.cleancode.org.domain.pack.functional.service;
 
 import fr.cleancode.org.domain.ApplicationError;
+import fr.cleancode.org.domain.pack.functional.exception.PaymentException;
 import fr.cleancode.org.domain.pack.functional.service.validation.PaymentValidator;
 import fr.cleancode.org.domain.player.functional.model.Player;
 
+import java.text.ParseException;
+
 public interface Buying {
-    default int payProduct(Player player, int price){
+    default void payProduct(Player player, int price) throws PaymentException{
         if(PaymentValidator.validate(player, price)){
-            return spendMoney(player, price);
+            int remainingBalance = spendMoney(player, price);
+            player.setToken(remainingBalance);
         }
         else {
-            //exception handle
-            return 99;
+            throw  new PaymentException("Payment invalidated");
         }
     }
 
