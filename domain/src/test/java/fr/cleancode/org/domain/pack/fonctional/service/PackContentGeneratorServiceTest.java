885f83e0f281eb826ccbd04e69a8ac29ef3ab34b
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-02-17 01:38:02 +0100

    development[feature/buyPack](clean-code-deck) probability test for heroGeneration in fonction of pack proba

--- domain/src/test/java/fr/cleancode/org/domain/pack/fonctional/service/PackContentGeneratorServiceTest.java
+++ domain/src/test/java/fr/cleancode/org/domain/pack/fonctional/service/PackContentGeneratorServiceTest.java
@@ -12,8 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -72,6 +71,52 @@ public class PackContentGeneratorServiceTest {
 
         assertThrows(IllegalArgumentException.class, () -> generatorService.generateContent(pack));
     }
+
+
+    @Test
+    public void testProba() {
+        List<Hero> heroesList = new ArrayList<>();
+        heroesList.add(Hero.builder().name("Hero1").rarity(Rarity.COMMON).build());
+        heroesList.add(Hero.builder().name("Hero2").rarity(Rarity.COMMON).build());
+        heroesList.add(Hero.builder().name("Hero3").rarity(Rarity.COMMON).build());
+        heroesList.add(Hero.builder().name("Hero4").rarity(Rarity.RARE).build());
+        heroesList.add(Hero.builder().name("Hero5").rarity(Rarity.RARE).build());
+        heroesList.add(Hero.builder().name("Hero6").rarity(Rarity.RARE).build());
+        heroesList.add(Hero.builder().name("Hero7").rarity(Rarity.LEGENDARY).build());
+        heroesList.add(Hero.builder().name("Hero8").rarity(Rarity.LEGENDARY).build());
+        heroesList.add(Hero.builder().name("Hero9").rarity(Rarity.LEGENDARY).build());
+        when(heroFinderApi.findAllHeroes()).thenReturn(heroesList);
+
+        int nbIterations = 10000;
+
+        Pack pack = PackType.SILVER.value;
+        pack.setProba(new int[]{50, 30, 20});
+
+        Map<Rarity, Integer> heroByRarity = new HashMap<>();
+        heroByRarity.put(Rarity.COMMON, 0);
+        heroByRarity.put(Rarity.RARE, 0);
+        heroByRarity.put(Rarity.LEGENDARY, 0);
+
+        for (int i = 0; i < nbIterations; i++) {
+            List<Hero> packContent = generatorService.generateContent(pack);
+            for (Hero hero : packContent) {
+                heroByRarity.put(hero.getRarity(), heroByRarity.get(hero.getRarity()) + 1);
+            }
+        }
+
+        int totalCommon = heroByRarity.get(Rarity.COMMON);
+        int totalRare = heroByRarity.get(Rarity.RARE);
+        int totalLegendary = heroByRarity.get(Rarity.LEGENDARY);
+
+        int expectedTotalCommun = nbIterations * pack.getCardsQuantity() * pack.getProba()[0] / 100;
+        int expectedTotalRare = nbIterations * pack.getCardsQuantity() * pack.getProba()[1] / 100;
+        int expectedTotalLegendary = nbIterations * pack.getCardsQuantity() * pack.getProba()[2] / 100;
+
+        assertEquals(expectedTotalCommun, totalCommon, nbIterations * 0.1);
+        assertEquals(expectedTotalRare, totalRare, nbIterations * 0.1);
+        assertEquals(expectedTotalLegendary, totalLegendary, nbIterations * 0.1);
+    }
+
 
 }
 
