c4b7a559f672100d867c0fb42f1cacf6fde24884
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-02-21 01:58:03 +0100

    development [feature/fight](clean-code-deck) begenning of patching findHeroById, optional

--- domain/src/main/java/fr/cleancode/org/domain/hero/ports/server/HeroFinderSpi.java
+++ domain/src/main/java/fr/cleancode/org/domain/hero/ports/server/HeroFinderSpi.java
@@ -3,8 +3,11 @@ package fr.cleancode.org.domain.hero.ports.server;
 import fr.cleancode.org.domain.hero.functional.model.Hero;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface HeroFinderSpi {
 
     List<Hero> findAllHeroes();
+
+    //Hero findHeroById(UUID id);
 }
