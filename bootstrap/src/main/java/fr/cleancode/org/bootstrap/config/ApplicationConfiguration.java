67af88edf92c98e705d4001fd60b4b7f8136cb10
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2023-02-27 15:31:52 +0100

    implementation[feature/implement-swagger](clean-code-deck) implementation of the swagger

--- bootstrap/src/main/java/fr/cleancode/org/bootstrap/config/ApplicationConfiguration.java
+++ bootstrap/src/main/java/fr/cleancode/org/bootstrap/config/ApplicationConfiguration.java
@@ -1,11 +1,12 @@
 package fr.cleancode.org.bootstrap.config;
 
 import fr.cleancode.org.bootstrap.config.domain.DomainConfiguration;
+import fr.cleancode.org.client.rest.config.SpringFoxConfig;
 import fr.cleancode.org.server.mongo.config.MongoServerAdapterConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({DomainConfiguration.class, MongoServerAdapterConfiguration.class})
+@Import({DomainConfiguration.class, MongoServerAdapterConfiguration.class, SpringFoxConfig.class})
 public class ApplicationConfiguration {
 }
